3ace2816eff482f750229b10308454b7b42d7884
Leslie P. Polzer <sky@havana.port-zero.com>
2018-12-17 01:16:09 +0100

    fixes and amendments.

--- process-spiritus-mail.sh
+++ process-spiritus-mail.sh
@@ -4,14 +4,27 @@
 set -x
 
 SCRIPT_HOME=$(pwd)/./$(dirname "$0")
-OUTDIR="$SCRIPT_HOME/../out_html/"$(date -Iseconds)
+OUTROOT="$SCRIPT_HOME/../out_html"
+OUTDIR="$OUTROOT/$(date -Iseconds)"
 RAWFILE="$OUTDIR/raw.eml"
 
 mkdir -p "$OUTDIR"
+cd "$OUTDIR"
 
 cp /dev/stdin "$RAWFILE"
 
-cd "$OUTDIR"
-HTMLFILE=$("$SCRIPT_HOME"/replace-inline-images.sh "$RAWFILE" "$OUTDIR" | grep HTMLFILE | cut -d: -f2-)
+TITLE=$(grep -E \^Subject: "$RAWFILE" | cut -d\[ -f3 | sed -e 's/[[:space:]]*$//')
+
+RAWFILE_TITLED='['"$TITLE.eml"
+mv "$RAWFILE" "$RAWFILE_TITLED"
+
+HTMLFILE=$("$SCRIPT_HOME"/replace-inline-images.sh "$RAWFILE_TITLED" "$OUTDIR" | grep HTMLFILE | cut -d: -f2-)
+HTMLFILE_TITLED="$OUTDIR"/'['"$TITLE.html"
 # TODO assert HTMLFILE captured
-"$SCRIPT_HOME"/sanitize-yahoo-html.py "$HTMLFILE" "$OUTDIR/$HTMLFILE.html"
+"$SCRIPT_HOME"/sanitize-yahoo-html.py "$HTMLFILE" "$HTMLFILE_TITLED"
+# TODO check if tree util installed
+cd "$OUTROOT"
+find . -type f -exec chmod go+r '{}' \;
+tar cfj archive.tar.bz2 * --exclude=\*.eml --exclude=\*.tar.bz2
+tree -Dtrfh -H . -I *.eml -P \*.html\|archive.tar.bz2 -T 'Spiritus Angel Messages' > index.html
+cp "$SCRIPT_HOME"/style.css "$OUTROOT" -v
--- sanitize-yahoo-html.py
+++ sanitize-yahoo-html.py
@@ -7,16 +7,22 @@ def process_html(fname, encoding):
     with open(argv[1], encoding=encoding) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
+        # just get the relevant body
         mlmsg_matches = soup.find_all('div', 'moz-forward-container')
         #mlmsg_matches = soup.find_all(id='ygrp-text')
         #assert(len(mlmsg_matches) == 1)
         mlmsg = mlmsg_matches[0]
 
+        # remove spurious table
+        mlmsg.find_all('table', 'moz-email-headers-table')[0].extract()
+
+        # keep existing CSS
         style_matches = soup.find_all('style')
         assert(len(style_matches) == 1)
         style = style_matches[0]
 
-        outdoc = '<html><head><link type="text/css" rel="stylesheet" href="style.css"/>%s</head><body>%s</body></html>' % (style, mlmsg)
+        # build and write new doc
+        outdoc = '<html><head><meta charset="UTF-8"/><link type="text/css" rel="stylesheet" href="/style.css"/>%s</head><body>%s</body></html>' % (style, mlmsg)
         
         with open(argv[2], 'w', encoding='utf-8') as outfp:
             outfp.write(outdoc)
@@ -26,7 +32,9 @@ def process_html(fname, encoding):
 assert(len(argv) == 3)
 
 try:
+    print('Trying to process as UTF-8...')
     process_html(argv[1], 'utf-8')
 except UnicodeDecodeError:
     # sometimes utf-8 is declared but contents are latin1
+    print('Failed to process as UTF-8, trying Latin1.')
     process_html(argv[1], 'iso-8859-1')
